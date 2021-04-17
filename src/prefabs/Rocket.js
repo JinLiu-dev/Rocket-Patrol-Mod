b870f01e40d3f073d953756ab652841f163743ce
jliu237 <jliu237@ucsc.edu>
2021-04-16 23:54:10 -0700

    mouse control for player

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,23 +5,39 @@ class Rocket extends Phaser.GameObjects.Sprite {
       scene.add.existing(this);
       this.movementSpeed = 2;
       this.isFiring = false;
+      this.mousemove = false;
+      
     }
     update(){
+        //console.log();
         if(this.isFiring) {
             this.y -= this.movementSpeed;
             if(this.y <= borderUISize * 3 + borderPadding) {
                 this.reset();
             }
         }
-
+        //console.log(this.scene.input.mousePointer.x);
         if(keyLEFT.isDown) {
+            this.mousemove = false;
             this.x -= this.movementSpeed;
             }
-            if(keyRIGHT.isDown) {
+        if(keyRIGHT.isDown) {
+            this.mousemove = false;
+            this.x += this.movementSpeed;
+            }
+        if(this.scene.input.mousePointer.velocity.x != 0){
+            this.mousemove = true;
+        }
+        if(this.mousemove){
+            if(this.scene.input.x - this.x > 0){
                 this.x += this.movementSpeed;
             }
+            if(this.scene.input.x - this.x < 0){
+                this.x -= this.movementSpeed;
+            }
+        }
 
-            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) || this.scene.input.mousePointer.isDown) {
                 this.isFiring = true;
                 this.sfxRocket.play();
             }
